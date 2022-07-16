3eee5f7691aa3f3d96e02c08cf11b7df4d7426eb
Eike mx <merxeike@gmail.com>
2022-07-16 11:41:32 +0200

    get owner

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ require("./database/client");
 const express = require ("express");
 const app = express();
 const userRouter = require('./routes/userRouter');
+const meRouter = require("./routes/meRouter");
 
 app.use(express.json());
 app.use("/users", userRouter);
+app.use("/me", meRouter);
 // app.get("/", (req, res) => {
 //     res.send(console.log("Yippi"));
 // })
